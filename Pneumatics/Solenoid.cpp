edd298a4e9ca050ee1b42cc70282e8486708d50b
ailyntong <athappiness@gmail.com>
2015-01-08 12:48:54 -0800

    fixed syntax errors and corrected constructor. changed justPressed to two separate variables

--- Pneumatics/Solenoid.cpp
+++ Pneumatics/Solenoid.cpp
@@ -18,13 +18,18 @@ class Solenoid : public IterativeRobot {
   private:
     DoubleSolenoid solenoid;
     Joystick stick;
-    bool justPressed; 
+    bool justPressedFR;	//forward/reverse
+    bool justPressedOF; //on/off
 
 };
 
 Solenoid::Solenoid():
 	solenoid((uint32_t)423, (uint32_t)432),
-	stick((uint32_t)423),
+	stick((uint32_t)423) 
+	{
+		justPressedFR = false;
+		justPressedOF = false;
+	}
 
 void Solenoid::RobotInit() {
 
@@ -56,23 +61,24 @@ void Solenoid::TeleopInit() {
 void Solenoid::TeleopPeriodic() {
 	//When you press button two it changes the solenoid value
 	
-	if(stick.GetRawButton((uint32_t)2) && !justPressed) {
-		if(solenoid.Get == DoubleSolenoid::Value::kForward) {
+	if(stick.GetRawButton((uint32_t)2) && !justPressedFR) {
+		if(solenoid.Get() == DoubleSolenoid::Value::kForward) {
 			solenoid.Set(DoubleSolenoid::Value::kReverse); 	
 		}
 		else if(solenoid.Get() == DoubleSolenoid::Value::kReverse) {
 			solenoid.Set(DoubleSolenoid::Value::kForward); 	
 		}
 	}
-  if(stick.GetRawButton((uint32_t)3) && !justPressed) {
-		if(solenoid.Get == DoubleSolenoid::Value::kOn) {
+  if(stick.GetRawButton((uint32_t)3) && !justPressedOF) {
+		if(solenoid.Get() == DoubleSolenoid::Value::kOn) {
 			solenoid.Set(DoubleSolenoid::Value::kOff); 	
 		}
 		else if(solenoid.Get() == DoubleSolenoid::Value::kOff) {
 			solenoid.Set(DoubleSolenoid::Value::kOn); 	
 		}
 	}
-	justPressed = stick.GetRawButton((uint32_t)2)||stick.GetRawButton((uint32_t)3);
+	justPressedFR = stick.GetRawButton((uint32_t)2);
+	justPressedOF = stick.GetRawButton((uint32_t)3);
 }
 
 void Solenoid::TeleopDisabled() {
