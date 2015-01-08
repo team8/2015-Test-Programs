575ce6f0a05d7085f68b733be499a991341c6191
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-01-08 15:43:47 -0800

    Restarting mecanum wheel project

---
+++ Mecanum Drive/src/Constants.h
@@ -0,0 +1,15 @@
+/*
+ * Constants.h
+ *
+ *  Created on: Jan 8, 2015
+ *      Author: EngTechP7
+ */
+
+#ifndef SRC_CONSTANTS_H_
+#define SRC_CONSTANTS_H_
+
+
+
+
+
+#endif /* SRC_CONSTANTS_H_ */
--- Mecanum Drive/src/Mecanum.h
+++ Mecanum Drive/src/Mecanum.h
@@ -6,24 +6,7 @@
 
 class Mecanum {
 public:
-	Mecanum();
-	void init();
-	void disable();
-	void update();
-	void meep(double linear, double curvy);
-	void set();
-private:
-	Talon leftFront;
-	Talon leftBack;
-	Talon rightFront;
-	Talon rightBack;
 
-	double linear;
-	double curvy;
-	double leftSpeed;
-	double rightSpeed;
-
-	enum State {ON, OFF} state;
 };
 
 #endif
