849ea7a9babd9c410c01f19c255d5375933b29e3
Nihar Mitra <nihar.roy.mitra@gmail.com>
2015-01-08 16:54:10 -0800

    Almost working code

--- Mecanum Drive/src/Constants.h
+++ Mecanum Drive/src/Constants.h
@@ -8,8 +8,14 @@
 #ifndef SRC_CONSTANTS_H_
 #define SRC_CONSTANTS_H_
 
+//ports for drive train talons
+#define TALON_LEFT_FRONT 5
+#define TALON_LEFT_BACK 6
+#define TALON_RIGHT_FRONT 1
+#define TALON_RIGHT_BACK 2
 
-
-
+//ports for joysticks
+#define DRIVE_STICK 1
+#define TURN_STICK 2
 
 #endif /* SRC_CONSTANTS_H_ */
---
+++ Mecanum Drive/src/Mecanum.cpp
@@ -0,0 +1,37 @@
+#include <WPILib.h>
+
+/**
+ * Simplest program to drive a robot with mecanum drive using a single Logitech
+ * Extreme 3D Pro joystick and 4 drive motors connected as follows:
+ *   - Digital Sidecar 1:
+ *     - PWM 1 - Connected to front left drive motor
+ *     - PWM 2 - Connected to rear left drive motor
+ *     - PWM 3 - Connected to front right drive motor
+ *     - PWM 4 - Connected to rear right drive motor
+ */
+class Mecanum: public IterativeRobot {
+private:
+	RobotDrive *robotDrive;
+	Joystick *driveStick;
+	Joystick *turnStick;
+public:
+	Mecanum();
+	/**
+	 * Constructor for this "MecanumDefaultCode" Class.
+	 */
+
+};
+Mecanum::Mecanum() :
+		driveStick(new Joystick((uint32_t) 4)), turnStick(
+				new Joystick((uint32_t) 2)) {
+	driveStick->SetAxisChannel(Joystick::kTwistAxis, 3);
+}
+/**
+ * Gets called once for each new packet from the DS.
+ */
+void TeleopPeriodic(void) {
+	robotDrive -> MecanumDrive_Cartesian(driveStick->GetX(), driveStick->GetY(),
+			turnStick->GetX());
+}
+
+START_ROBOT_CLASS(Mecanum);
