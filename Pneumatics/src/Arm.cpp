aa4fb3f03b3b90123582fb7c2295f93e0c0d3f01
Eric Liu <ericxliu@gmail.com>
2015-01-11 14:51:51 -0800

    made changes

--- Pneumatics/src/Arm.cpp
+++ Pneumatics/src/Arm.cpp
@@ -1,3 +1,17 @@
+#include <CompressorThing.cpp>
 #include <WPILib.h>
 #include <iostream>
+#include "SolenoidRobot.cpp"
+#include "Arm.h"
 
+
+void Arm::setState(State state)
+{
+
+}
+
+void Arm::disable()
+{
+	solenoid.Set(DoubleSolenoid::Value::kOff);
+	compressor.Stop();
+}
